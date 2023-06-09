7c88e7a277e198f60d63cec5a4232d44acb11711
michel.fortes <michel.fortes@softplan.com.br>
2023-06-09 10:58:03 -0300

    changed URI to URL in DTO

--- src/main/java/br/com/teste/httpbindcoker/RequestInfoDto.java
+++ src/main/java/br/com/teste/httpbindcoker/RequestInfoDto.java
@@ -4,12 +4,12 @@ public class RequestInfoDto {
 
     public final String tenant;
     public final String method;
-    public final String uri;
+    public final String url;
 
-    public RequestInfoDto(String tenant, String method, String uri) {
+    public RequestInfoDto(String tenant, String method, String url) {
         this.tenant = tenant;
         this.method = method;
-        this.uri = uri;
+        this.url = url;
     }
 
 }
