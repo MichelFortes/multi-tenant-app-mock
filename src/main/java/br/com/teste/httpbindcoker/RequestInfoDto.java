cbf55f3f314be4e92d14e45635e5152881904120
michel.fortes <michel.fortes@softplan.com.br>
2023-06-12 08:56:33 -0300

    Response semantics improved

--- src/main/java/br/com/teste/httpbindcoker/RequestInfoDto.java
+++ src/main/java/br/com/teste/httpbindcoker/RequestInfoDto.java
@@ -2,12 +2,12 @@ package br.com.teste.httpbindcoker;
 
 public class RequestInfoDto {
 
-    public final String tenant;
+    public final String container;
     public final String method;
     public final String url;
 
-    public RequestInfoDto(String tenant, String method, String url) {
-        this.tenant = tenant;
+    public RequestInfoDto(String container, String method, String url) {
+        this.container = container;
         this.method = method;
         this.url = url;
     }
