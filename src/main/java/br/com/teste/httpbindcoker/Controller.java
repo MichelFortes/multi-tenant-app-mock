c9461066bb16a74d85f90318e284b220e7902144
michel.fortes <michel.fortes@softplan.com.br>
2023-08-23 10:00:48 -0300

    Mudei para webflux para melhorar a performance

--- src/main/java/br/com/teste/httpbindcoker/Controller.java
+++ src/main/java/br/com/teste/httpbindcoker/Controller.java
@@ -4,10 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
@@ -17,12 +16,12 @@ public class Controller {
     private String container;
 
     @RequestMapping(value = "/**")
-    public ResponseEntity<ResponseDto> customer(HttpServletRequest request) {
+    public ResponseEntity<ResponseDto> customer(ServerHttpRequest request) {
         return body(request);
     }
 
-    private ResponseEntity<ResponseDto> body(HttpServletRequest request) {
-        ResponseDto dto = new ResponseDto(container, request.getMethod(), request.getRequestURL().toString());
+    private ResponseEntity<ResponseDto> body(ServerHttpRequest request) {
+        ResponseDto dto = new ResponseDto(container, request.getMethod().toString(), request.getURI().toString());
         return ResponseEntity.status(HttpStatus.OK).body(dto);
     }
 
--- src/main/java/br/com/teste/httpbindcoker/HttpbinDockerApplication.java
+++ src/main/java/br/com/teste/httpbindcoker/HttpbinDockerApplication.java
@@ -2,8 +2,10 @@ package br.com.teste.httpbindcoker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class HttpbinDockerApplication {
 
 	public static void main(String[] args) {
